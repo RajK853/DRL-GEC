f8276d8afd2be8aba392a718a3070628f6a19d84
Raj K <rgk.rana@gmail.com>
2022-10-31 13:11:11 +0100

    ref: Minor changes
    - Removing the final pooling layer from the transformer encoder
    - Add docstrings and type hints

--- src/models/seq2labels.py
+++ src/models/seq2labels.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from typing import Dict, List, Tuple, Any
+from typing import Dict, List, Any
 from transformers import AutoTokenizer, AutoConfig, AutoModel
 from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
 
@@ -9,38 +9,43 @@ from src.utils import stack_padding, START_TOKEN
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-cache_dir = r"~/.cache/huggingface"
+DEFAULT_TOKENIZER_CFG = {"use_fast": True}
+DEFAULT_TRANSFORMER_CONFIG = {"output_attentions": False}
 
 
 class PretrainedEncoder(nn.Module):
-    def __init__(self, model_name: str, tokenizer_config: Dict[str, Any] = None, transformer_config: Dict[str, Any] = None):
+    def __init__(
+            self,
+            model_name: str,
+            tokenizer_config: Dict[str, Any] = None,
+            transformer_config: Dict[str, Any] = None,
+            add_pooling_layer: bool = False,
+    ):
         super(PretrainedEncoder, self).__init__()
-        tokenizer_config = tokenizer_config or {"use_fast": True}
-        transformer_config = transformer_config or {"output_attentions": False}
         # Init tokenizer and transformer
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_config, cache_dir=cache_dir)
-        self.config = AutoConfig.from_pretrained(model_name, **transformer_config, cache_dir=cache_dir)
-        self.transformer = AutoModel.from_pretrained(model_name, config=self.config, cache_dir=cache_dir)
-        # Remove the last pooler layer as it is not present in AutoModelForTokenClassification
-        self.transformer.pooler = None
+        tokenizer_config = tokenizer_config or DEFAULT_TOKENIZER_CFG.copy()
+        transformer_config = transformer_config or DEFAULT_TRANSFORMER_CONFIG.copy()
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_config, local_files_only=True)
+        self.config = AutoConfig.from_pretrained(model_name, **transformer_config)
+        self.transformer = AutoModel.from_pretrained(
+                model_name, config=self.config, local_files_only=True, add_pooling_layer=add_pooling_layer)
         self.return_offsets_mapping = isinstance(self.tokenizer, PreTrainedTokenizerFast)
-        # self.add_token(START_TOKEN)
+        self.add_tokens([START_TOKEN])
 
-    def add_token(self, token: str):
-        self.tokenizer.add_tokens([token])
-        self.tokenizer.vocab[token] = len(self.tokenizer) - 1
-
-    def forward(self, tokens: List[str]) -> torch.Tensor:
-        token_data = self.tokenize_batch(tokens)
-        aligns = self.get_alignments(token_data["offset_mapping"], token_data["lengths"])
-        trf_out = self.transformer(
-                input_ids=token_data["input_ids"].to(device),
-                attention_mask=token_data["attention_mask"].to(device),
-        )
-        encoded_out = reduce_mean(trf_out.last_hidden_state, aligns).to(device)
-        return encoded_out
+    def add_tokens(self, tokens: List[str]):
+        """
+        Add tokens to the tokenizer and resize the transformer embeddings
+        """
+        token_vocab = self.tokenizer.get_vocab().keys()
+        new_tokens = [token for token in tokens if token not in token_vocab]
+        if new_tokens:
+            self.tokenizer.add_tokens(new_tokens)
+            self.transformer.resize_token_embeddings(len(self.tokenizer))
 
     def tokenize_batch(self, tokens: List[str]) -> Dict[str, Any]:
+        """
+        Tokenize batch of strings into
+        """
         token_data = self.tokenizer(
                 [" ".join(toks) for toks in tokens],
                 add_special_tokens=False,
@@ -49,12 +54,15 @@ class PretrainedEncoder(nn.Module):
                 return_tensors="pt",
                 return_token_type_ids=None,
                 padding="longest",
-        )
+        ).to(device)
         token_data["lengths"] = [[len(tok) for tok in toks] for toks in tokens]
         return token_data
 
     @staticmethod
     def get_alignments(offsets: torch.Tensor, token_lens: List[List[int]]) -> torch.Tensor:
+        """
+        Get alignments used to keep track of splitted tokens
+        """
         aligns = []
         batch_size, pad_size = offsets.shape[:2]
         offset_lens = offsets[:, :, 1] - offsets[:, :, 0]      # Obtain offset lengths by subtracting start-end indexes
@@ -75,11 +83,24 @@ class PretrainedEncoder(nn.Module):
         padded_aligns = stack_padding(aligns, dtype="int32")   # Pad the aligns as int32
         return torch.from_numpy(padded_aligns)
 
+    def forward(self, tokens: List[str]) -> torch.Tensor:
+        """
+        Forward function
+        """
+        token_data = self.tokenize_batch(tokens)
+        aligns = self.get_alignments(token_data["offset_mapping"], token_data["lengths"])
+        trf_out = self.transformer(
+                input_ids=token_data["input_ids"],
+                attention_mask=token_data["attention_mask"],
+        )
+        encoded_out = reduce_mean(trf_out.last_hidden_state, aligns)
+        return encoded_out
+
 
 class Seq2Labels(nn.Module):
     def __init__(self, encoder_model: PretrainedEncoder, num_labels: int, dropout: float = 0.1):
         super(Seq2Labels, self).__init__()
-        self.encoder_model = encoder_model.to(device)
+        self.encoder = encoder_model.to(device)
         encoder_output_size = encoder_model.config.hidden_size
         self.classifier = nn.Sequential(
                 nn.Dropout(p=dropout),
@@ -87,28 +108,25 @@ class Seq2Labels(nn.Module):
         ).to(device)
 
     def forward(self, tokens: List[str]) -> torch.Tensor:
-        encoded_out = self.encoder_model(tokens)
+        encoded_out = self.encoder(tokens)
         logit_out = self.classifier(encoded_out)
         return logit_out
 
 
-class Seq2LabelsV2(nn.Module):
-    def __init__(self, encoder_model: PretrainedEncoder, num_categories: int, num_labels: int, dropout: float = 0.1):
-        super(Seq2LabelsV2, self).__init__()
-        self.encoder_model = encoder_model.to(device)
+class Seq2LabelsDeeper(nn.Module):
+    def __init__(self, encoder_model: PretrainedEncoder, num_labels: int, dropout: float = 0.1):
+        super(Seq2LabelsDeeper, self).__init__()
+        self.encoder = encoder_model.to(device)
         encoder_output_size = encoder_model.config.hidden_size
-        self.category_classifier = nn.Sequential(
+        self.classifier = nn.Sequential(
                 nn.Dropout(p=dropout),
-                nn.Linear(encoder_output_size, num_categories),
-                nn.Softmax(dim=-1),
-        ).to(device)
-        self.label_classifier = nn.Sequential(
-                nn.Dropout(p=dropout),
+                nn.Linear(encoder_output_size, encoder_output_size),
+                nn.ReLU(),
                 nn.Linear(encoder_output_size, num_labels),
         ).to(device)
 
-    def forward(self, tokens: List[str]) -> Tuple[torch.Tensor, torch.Tensor]:
-        encoded_out = self.encoder_model(tokens)
-        labels_logit_out = self.label_classifier(encoded_out)
-        categories_logit_out = self.category_classifier(encoded_out)
-        return labels_logit_out, categories_logit_out
+    def forward(self, tokens: List[str]) -> torch.Tensor:
+        encoded_out = self.encoder(tokens)
+        logit_out = self.classifier(encoded_out)
+        return logit_out
+
