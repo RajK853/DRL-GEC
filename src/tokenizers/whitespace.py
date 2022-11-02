fce4bf222593e04f78f581826a7b6a3c7adf3846
Raj K <rgk.rana@gmail.com>
2022-11-03 00:15:34 +0100

    ref: Add docstring and type hints

--- src/tokenizers/whitespace.py
+++ src/tokenizers/whitespace.py
@@ -1,11 +1,22 @@
-from .base_tokenizer import Tokenizer
+from typing import List
+
+from .base import Tokenizer
 
 
 class WSTokenizer(Tokenizer):
+    """
+    Whitespace-based tokenizer
+    """
     @staticmethod
-    def text2tokens(text):
+    def text2tokens(text: str) -> List[str]:
+        """
+        Convert a text into tokens
+        """
         return text.split()
 
     @staticmethod
-    def tokens2text(tokens):
+    def tokens2text(tokens: List[str]) -> str:
+        """
+        Convert tokens into a text
+        """
         return " ".join(tokens)
