e1d4065c31930032b3ffd03df8357a4f9461adc3
Raj K <rgk.rana@gmail.com>
2022-11-09 13:05:41 +0100

    ref: Minor changes
    - `get_best_candidates` returns candidate labels and their corresponding change in distances
    - Update `get_best_label` algorithm
      - Add `$KEEP` label in the candidate labels
      - Exploit = Sampling from candidate labels using mean(candidate_delta_probs, policy_probs)

--- src/planning.py
+++ src/planning.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 
-from src.utils import apply_labels_at, get_lev_dist
+from src.utils import apply_labels_at, get_lev_dist, softmax
 
 
 def get_lev_dist_of_next_tokens(tokens, reference, tok_i, tok_label):
@@ -13,32 +13,37 @@ def get_lev_dist_of_next_tokens(tokens, reference, tok_i, tok_label):
 def get_best_candidates(mask_gen, tokens, reference, tok_i, tok_edit, src_ref_lev_dist, verbose=False):
     tok_labels = mask_gen.edit_to_labels(tok_edit)
     distances = np.array([get_lev_dist_of_next_tokens(tokens, reference, tok_i, tok_label) for tok_label in tok_labels])
-    candidate_mask = distances < src_ref_lev_dist
+    delta = distances - src_ref_lev_dist
+    candidate_mask = delta < 0
     candidate_labels = tok_labels[candidate_mask]
+    candidate_delta = np.abs(delta[candidate_mask])     # Ignore sign as we are only interested in magnitude
     if verbose:
         print(f"Candidate labels for the token `{tokens[tok_i]}` at index `{tok_i}`: {candidate_labels}")
-    return candidate_labels
+    return candidate_labels, candidate_delta
 
 
-def get_best_label(policy, mask_gen, tokens, tok_i, candidate_labels, explore=True):
+def get_best_label(policy, mask_gen, tokens, tok_i, candidate_labels, candidate_delta, explore=True):
     num_candidates = len(candidate_labels)
     if num_candidates == 0:
-        best_tok_label = "$KEEP"
-    elif num_candidates == 1:
-        best_tok_label = candidate_labels[0]
+        return "$KEEP"
+    # Insert $KEEP label to give chance to not modify the token
+    candidate_labels = np.append(candidate_labels, "$KEEP")
+    candidate_delta = np.append(candidate_delta, 0)
+    if explore:
+        candidate_probs = None
     else:
-        candidate_indexes = mask_gen.labels_to_actions(list(candidate_labels))
-        if explore:
-            candidate_probs = None            # Use uniform sampling
-        else:
-            with torch.no_grad():
-                [logits] = policy([tokens])
-                candidate_probs = logits[tok_i, candidate_indexes].softmax(0).cpu().numpy()
-        best_tok_label = np.random.choice(candidate_labels, p=candidate_probs)
+        with torch.no_grad():
+            [logits] = policy([tokens])
+            candidate_indexes = mask_gen.labels_to_actions(list(candidate_labels))
+            policy_probs = logits[tok_i, candidate_indexes].softmax(0).cpu().numpy()
+            delta_probs = softmax(candidate_delta)
+            candidate_probs = 0.5*(policy_probs + delta_probs)  # Calculate elementwise mean between two probabilities
+            candidate_probs /= candidate_probs.sum()    # Normalize to make sure sum(probs) is exactly 1.0
+    best_tok_label = np.random.choice(candidate_labels, p=candidate_probs)
     return best_tok_label
 
 
-def search_best_actions(policy, tokens, mask_gen, reference, explore=True, verbose=False):
+def search_best_actions(policy, tokens, reference, mask_gen, explore=True, verbose=False):
     # Search labels
     num_tokens = len(tokens)
     labels = ["$KEEP"]*num_tokens
@@ -47,17 +52,18 @@ def search_best_actions(policy, tokens, mask_gen, reference, explore=True, verbo
         edit_mask = mask_gen.get_edit_mask(tokens, [reference])
         if verbose:
             print(f"Edit_mask: {edit_mask}")
-        for tok_i in reversed(range(num_tokens)):
-            tok_edit = edit_mask[tok_i]
+        for tok_i, tok_edit in reversed(tuple(enumerate(edit_mask))):
             if tok_edit == "equal":
                 continue
-            elif tok_edit == "delete":
-                tok_label = "$DELETE"
-            else:
-                candidate_labels = get_best_candidates(mask_gen, tokens, reference, tok_i, tok_edit, current_lev_dist, verbose=False)
-                tok_label = get_best_label(policy, mask_gen, tokens, tok_i, candidate_labels, explore=explore)
-            tokens = apply_labels_at(tokens, [tok_label], [tok_i])
-            current_lev_dist = get_lev_dist(tokens, reference)
-            labels[tok_i] = tok_label
+            candidate_labels, candidate_delta = get_best_candidates(
+                    mask_gen, tokens, reference, tok_i, tok_edit, current_lev_dist, verbose=verbose
+            )
+            tok_label = get_best_label(
+                    policy, mask_gen, tokens, tok_i, candidate_labels, candidate_delta, explore=explore
+            )
+            if tok_label != "$KEEP":
+                tokens = apply_labels_at(tokens, [tok_label], [tok_i])
+                current_lev_dist = get_lev_dist(tokens, reference)
+                labels[tok_i] = tok_label
     actions = mask_gen.labels_to_actions(labels)
     return actions
